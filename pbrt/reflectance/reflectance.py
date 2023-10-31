aec2e58a5983266ff4fec88c89d31460c5df9b44
hescul <ndminh1101@gmail.com>
2023-10-31 23:04:28 +0100

    Add cell to export reflectance data to `.csv`

--- pbrt/reflectance/reflectance.py
+++ pbrt/reflectance/reflectance.py
@@ -14,14 +14,14 @@ def reflectance(n1, n2, inc_deg=45):
     return (refl_s + refl_p) / 2
 
 
-def spectral_reflectance(mat_idx, env_idx, inc_deg=45):
-    lambda_count = len(mat_idx)
+def spectral_reflectance(mat_indices, env_indices, inc_deg=45):
+    lambda_count = len(mat_indices)
 
-    if lambda_count != len(env_idx):
+    if lambda_count != len(env_indices):
         raise ValueError("Range of mat and env indices must be the same.")
 
-    eps_mat = np.array(mat_idx)
-    eps_env = np.array(env_idx)
+    eps_mat = np.array(mat_indices)
+    eps_env = np.array(env_indices)
 
     theta_i = np.deg2rad(inc_deg)
     theta_t = np.sqrt(1 - (np.divide(eps_env, eps_mat) * np.sin(theta_i)) ** 2)
